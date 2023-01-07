3d0cae8c1f9fb69e8f28d83a1a0c908fe02c5fa1
Tochi Ukegbu <tochiukegbu@gmail.com>
2023-01-07 17:03:22 -0500

    fix: audio is more accurate

--- src/run/audio.rs
+++ src/run/audio.rs
@@ -4,16 +4,15 @@ use rodio::{source::Source, OutputStream, Sink};
 
 use std::{
     sync::{
+        atomic::{AtomicBool, AtomicU32, AtomicU64, AtomicU8, Ordering},
         mpsc::{channel, RecvTimeoutError, Sender},
-        Arc, Mutex,
+        Arc
     },
     thread::JoinHandle,
     time::{Duration, Instant},
 };
 
 pub const AUDIO_BUFFER_SIZE_BYTES: usize = 16;
-
-const AUDIO_BUFFER_SIZE_BITS: usize = AUDIO_BUFFER_SIZE_BYTES * u8::BITS as usize;
 
 const BASE_SAMPLE_RATE: u32 = 4000;
 const DEFAULT_RECV_TIMEOUT: Duration = Duration::from_millis(1000);
@@ -43,41 +42,53 @@ impl From<RomKind> for Audio {
     }
 }
 
-pub struct AudioSourceData {
-    buffer: [f32; AUDIO_BUFFER_SIZE_BITS],
-    sample_rate: u32,
-    playback_offset: usize,
-    volume: f32,
-}
-
-impl AudioSourceData {
-    fn set_buffer(&mut self, buffer: &[u8; AUDIO_BUFFER_SIZE_BYTES]) {
-        self.buffer.iter_mut().enumerate().for_each(|(i, sample)| {
-            *sample = if buffer[i / 8] >> (7 - i % 8) & 1 == 1 {
-                1.0
-            } else {
-                0.0
-            }
-        });
-    }
-}
-
+#[derive(Clone)]
 pub struct AudioSource {
-    data: Arc<Mutex<AudioSourceData>>,
+    buffer: [Arc<AtomicU64>; 2],
+    playback_offset: Arc<AtomicU8>,
+    is_audible: Arc<AtomicBool>,
+    sample_rate: Arc<AtomicU32>,
 }
 
 impl AudioSource {
-    pub fn new(data: Arc<Mutex<AudioSourceData>>) -> Self {
-        data.lock()
-            .expect("Unable to lock audio source data")
-            .playback_offset = 0;
-        AudioSource { data }
+    pub fn new() -> Self {
+        AudioSource {
+            buffer: [Arc::new(AtomicU64::new(0)), Arc::new(AtomicU64::new(0))],
+            playback_offset: Arc::new(AtomicU8::new(0)),
+            is_audible: Arc::new(AtomicBool::new(false)),
+            sample_rate: Arc::new(AtomicU32::new(BASE_SAMPLE_RATE)),
+        }
+    }
+}
+
+impl AudioSource {
+    fn set_buffer(&self, buffer: &[u8; AUDIO_BUFFER_SIZE_BYTES]) {
+        buffer
+            .chunks_exact(8)
+            .map(|bytes| {
+                bytes
+                    .iter()
+                    .fold(0, |bit64, byte| (bit64 << 8) | *byte as u64)
+            })
+            .zip(self.buffer.iter())
+            .for_each(|(bit64, atomic64)| {
+                atomic64.store(bit64, Ordering::Release);
+            });
+    }
+    fn set_audible(&self, is_audible: bool) {
+        if is_audible {
+            self.playback_offset.store(0, Ordering::Release);
+        }
+        self.is_audible.store(is_audible, Ordering::Release);
+    }
+    fn set_sample_rate(&self, sample_rate: u32) {
+        self.sample_rate.store(sample_rate, Ordering::Release);
     }
 }
 
 impl Source for AudioSource {
     fn current_frame_len(&self) -> Option<usize> {
-        Some(4)
+        Some(8)
     }
 
     fn channels(&self) -> u16 {
@@ -85,10 +96,7 @@ impl Source for AudioSource {
     }
 
     fn sample_rate(&self) -> u32 {
-        self.data
-            .lock()
-            .expect("Unable to lock audio source data")
-            .sample_rate
+        self.sample_rate.load(Ordering::Acquire)
     }
 
     fn total_duration(&self) -> Option<Duration> {
@@ -100,10 +108,17 @@ impl Iterator for AudioSource {
     type Item = f32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut data = self.data.lock().expect("Unable to lock audio source data");
-        let sample = data.volume*data.buffer[data.playback_offset];
-        data.playback_offset = (data.playback_offset + 1) % data.buffer.len();
-        Some(sample)
+        let playback_offset = self.playback_offset.fetch_add(1, Ordering::SeqCst) % 128;
+        if self.is_audible.load(Ordering::Acquire) {
+            let is_waveform_peak = self.buffer[playback_offset as usize / 64]
+                .load(Ordering::Acquire)
+                >> (63 - (playback_offset % 64))
+                & 1 
+                == 1;
+            Some(if is_waveform_peak { 1.0 } else { 0.0 })
+        } else {
+            Some(0.0)
+        }
     }
 }
 
@@ -126,12 +141,7 @@ pub fn spawn_audio_thread() -> (Sender<AudioEvent>, JoinHandle<()>) {
 
         sink.set_volume(DEFAULT_VOLUME);
 
-        let data = Arc::new(Mutex::new(AudioSourceData {
-            buffer: [0.0; AUDIO_BUFFER_SIZE_BITS],
-            sample_rate: BASE_SAMPLE_RATE,
-            playback_offset: 0,
-            volume: 0.0,
-        }));
+        let source = AudioSource::new();
 
         let mut paused = false;
         let mut is_silent = true;
@@ -141,7 +151,7 @@ pub fn spawn_audio_thread() -> (Sender<AudioEvent>, JoinHandle<()>) {
         let mut recv_last_instant = Instant::now();
         let mut recv_timeout = DEFAULT_RECV_TIMEOUT;
 
-        sink.append(AudioSource::new(Arc::clone(&data)));
+        sink.append(source.clone());
         sink.play();
 
         loop {
@@ -166,9 +176,7 @@ pub fn spawn_audio_thread() -> (Sender<AudioEvent>, JoinHandle<()>) {
                         remaining_duration = duration;
                         if !remaining_duration.is_zero() {
                             if is_silent {
-                                data.lock()
-                                    .expect("Unable to lock audio source data")
-                                    .volume = 1.0;
+                                source.set_audible(true);
                                 is_silent = false;
                             }
                         }
@@ -179,14 +187,13 @@ pub fn spawn_audio_thread() -> (Sender<AudioEvent>, JoinHandle<()>) {
                         }
 
                         current_buffer = buffer;
-                        data.lock()
-                            .expect("Unable to lock audio source data")
-                            .set_buffer(&buffer);
+                        source.set_buffer(&buffer);
                     }
                     AudioEvent::SetPitch(pitch) => {
-                        data.lock().unwrap().sample_rate = (BASE_SAMPLE_RATE as f32
-                            * 2.0_f32.powf((pitch as f32 - 64.0) / 48.0))
-                        .round() as u32;
+                        source.set_sample_rate(
+                            (BASE_SAMPLE_RATE as f32 * 2.0_f32.powf((pitch as f32 - 64.0) / 48.0))
+                                .round() as u32,
+                        );
                     }
                     AudioEvent::Pause => 'guard: {
                         if paused {
@@ -215,9 +222,7 @@ pub fn spawn_audio_thread() -> (Sender<AudioEvent>, JoinHandle<()>) {
                 if paused || is_silent {
                     DEFAULT_RECV_TIMEOUT
                 } else if remaining_duration.is_zero() {
-                    data.lock()
-                        .expect("Unable to lock audio source data")
-                        .volume = 0.0;
+                    source.set_audible(false);
                     is_silent = true;
                     DEFAULT_RECV_TIMEOUT
                 } else {
